d0fa97d2e76d528795e4caa6cf68e2306d8573ff
emilia344 <gnanka@gmail.com>
2025-11-26 21:00:24 +0100

    emilia

---
+++ day07/classwork/class01.py
@@ -0,0 +1,6 @@
+# Debogag est quell que chose ou on se trompe sent la vior.
+
+# Le programme pour s eparer les est de ecrir un tire du bas. 
+
+# Le faite que python est s emsible est quand on ecri avec une petit lettre et apres avec une grand lettre.
+
---
+++ day07/homework/home01.py
@@ -0,0 +1,9 @@
+name = "gvanca"
+print(name)
+
+age = 12
+print(age)
+
+age = 5
+name = "dog"
+print(age)
