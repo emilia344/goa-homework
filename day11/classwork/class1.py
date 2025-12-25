bb8770a9015eb77f5cd1062acca815908f694417
emilia344 <gnanka@gmail.com>
2025-12-25 19:15:45 +0100

    emilia

---
+++ day11/classwork/class1.py
@@ -0,0 +1,32 @@
+
+#1.  ras aketebs type() instruqcia? 
+
+#type instruction est fait pour qu'on s'ache si c'est int,str...
+
+#sami monacemis tipis gamoyenebit (string, integer, float) gamoiyene type() intruqcia
+
+print(type(20))
+print(type(16.4))
+print(type("emilia"))
+
+#ras ektebs int(), str(), float(), bool()? moiyvane titoeulis magaliti meotxe davalebashi
+
+#int() sait un nombre entier
+#str() sait quell que chose qu'on met dans ca "" ou ca ''
+#float()est un nombre en fraction
+#bool()a de importensse c'est true et false
+
+num = 16
+print(str(num))
+
+num1 = 16.5
+print(int(num1))
+
+num2 = 16
+print(float(num2))
+
+num3 = 1
+print(bool(num3))
+
+num4 = 0
+print(bool(num4))
---
+++ home01.py
@@ -0,0 +1,112 @@
+#ra aris implicit da explicit?
+
+#Implicit aris roca kompiuteri cvlis avtumaturat monacems
+
+#explicia aris roca chven tviton vcvlit monacems
+
+#10-10 magaliti moiyvane int(), float(), str() monacemis tipis gardaqmnaze
+
+num = "pere noel"
+print(str(num))
+
+num1 = "lutin"
+print(str(num1))
+
+num2 = "noel"
+print(str(num2))
+
+num3 = "sapin"
+print(str(num3))
+
+num4 = "mere noel"
+print(str(num4))
+
+num5 = "rein"
+print(str(num5))
+
+num6 = "cadeau"
+print(str(num6))
+
+num7 = "sucre dorge"
+print(str(num7))
+
+num8 = "flocon"
+print(str(num8))
+
+num9 = "neig"
+print(str(num9))
+
+num10 = "pole nord"
+print(str(num10))
+
+#int
+
+num1 = 24
+print(int(num1))
+
+num2 = 17
+print(int(num2))
+
+num3 = 18
+print(int(num3))
+
+num4 = 5
+print(int(num4))
+
+num5 = 2
+print(int(num5))
+
+num6 = 31
+print(int(num6))
+
+num7 = 111
+print(int(num7))
+
+num8 = 2015
+print(int(num8))
+
+num9 = 89
+print(int(num9))
+
+num10 = 13
+print(int(num10))
+
+#float
+
+num1 = 123.4
+print(float(num1))
+
+num2 = 10.1
+print(float(num2))
+
+num3 = 201.5
+print(float(num3))
+
+num4 = 304.3
+print(float(num4))
+
+num5 = 11.5
+print(float(num5))
+
+num6 = 6.7
+print(float(num6))
+
+num7 = 18.4
+print(float(num7))
+
+num8 = 23.4
+print(float(num8))
+
+num9 = 56.8
+print(float(num9))
+
+num10 = 45.6
+print(float(num10))
+
+#type() instruqciis gamoyenebit sheamowme monacemis otxive tipi
+
+print(type("Emlia"))
+print(type(17))
+print(type(201.5))
+print(type(False))
+print(type(True))
