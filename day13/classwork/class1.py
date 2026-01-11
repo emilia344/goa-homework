d85bfb63c0241bb7a93be03ca95a70b441e9aaf5
emilia344 <gnanka@gmail.com>
2026-01-11 12:02:14 +0100

    ემილია

---
+++ day13/classwork/class1.py
@@ -0,0 +1,40 @@
+
+#1. gadaaqcia ricxvi 99 floatad da daprinte
+
+print(99)
+print(float(99))
+
+#gadaaqcia 12.5 integerad da daprinte
+
+print(12.5)
+print(int(12.5))
+
+#sheamowme booleanit 4 < 3
+
+print(bool(4<3))
+
+#input()-is gamoyenebit momxmarebels shemoatanine tavisi saxeli da gvari da daprinte
+prenom_nom=print(input("Ecri mon prenom et mon nom de famille"))
+print(prenom_nom)
+
+#type()- funqciit sheamowme ra monacemis tipebia 12, "Emilia", 160.0, True
+
+print(type(12))
+print(type("emilia"))
+print(type(160.0))
+print(type(True))
+
+#sheqmeni ori cvlade (a, b). mat mieci mnishvneloba integeri. shemdeg es integerebi: gayavi, gaamravle, miumate da gamoakle, es yvelaferi print()-shi chasvi
+
+a = 18
+b = 14
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+
+
+
+
+
+
