c40691c984d6abbb0b246e0b70c3f31fe636e727
emilia344 <gnanka@gmail.com>
2026-03-07 15:31:34 +0100

    emilia

---
+++ day21/classwork/c,lass2.py
@@ -0,0 +1,8 @@
+# ra tipis kodebi arsebobs? ganmarte sheni sityvebit
+
+# selection
+# selection veux dir tu a le chios entre la fraise et la ceris
+#iteration
+#iteration veux dir que ca repete la meme chose
+#sequences
+#sequences veux dir que ca lis de haut en bas
---
+++ day21/classwork/class1.py
@@ -0,0 +1,8 @@
+# moiyvane algorithmis 2 magaliti, programirebashi da namdvil cxovrebashi 
+
+#pizza
+#(1) Tu va prendre la patte et tu va l'ettaler
+#(2) tu va metre la sauce tomate et tu va l'etaler sur la pizza 
+
+age=10
+print(age)
