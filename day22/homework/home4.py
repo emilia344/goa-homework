2f4049c061ff1c3d644390350730ce2a6f01cc39
emilia344 <gnanka@gmail.com>
2026-03-15 10:29:45 +0100

    emilia

--- day22/homework/home4.py
+++ day22/homework/home4.py
@@ -1,6 +1,6 @@
 #Écrivez un programme utilisant une boucle for qui affiche les nombres de 50 à 20, en excluant les nombres 3 et 3
 
-for i in range(20 , 50 , -3):
+for i in range(50 , 20 , -3):
     print(i)
     
 
